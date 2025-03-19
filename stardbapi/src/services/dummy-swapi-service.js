30543561c705c9130f68205a92c2bedb8467dc46
Sania Maripova <niabmari@gmail.com>
2025-03-19 11:12:34 +0500

    15. Hoc to work with context

--- stardbapi/src/services/dummy-swapi-service.js
+++ stardbapi/src/services/dummy-swapi-service.js
@@ -16,7 +16,7 @@ export default class DummySwapiService {
             birthYear: 'long ago',
             eyeColor: 'dark brown'
         }
-    ];
+    ]
 
     _planets = [
         {
@@ -33,7 +33,7 @@ export default class DummySwapiService {
             rotationPeriod: '243 days',
             diameter: '12.104 km'
         }
-    ];
+    ]
 
     _starships = [
         {
@@ -47,41 +47,41 @@ export default class DummySwapiService {
             passengers: 50,
             cargoCapacity: 100
         }
-    ];
+    ]
 
     getAllPeople = async () => {
         return this._people;
-    };
+    }
 
     getPerson = async () => {
         return this._people[0];
-    };
+    }
 
     getAllPlanets = async () => {
         return this._planets;
-    };
+    }
 
     getPlanet = async () => {
         return this._planets[0]
-    };
+    }
 
     getAllStarships = async () => {
         return this._starships;
-    };
+    }
 
     getStarship = async () => {
         return this._starships[0];
-    };
+    }
 
     getPersonImage = () => {
         return `https://placeimg.com/400/500/people`
-    };
+    }
 
     getStarshipImage = () => {
         return `https://placeimg.com/600/400/tech`;
-    };
+    }
 
     getPlanetImage = () => {
         return `https://placeimg.com/400/400/nature`
-    };
+    }
 }
